92fe8c32b00ab0dfb4126042573a29b593d16d60
Evan Jeffries <ejeffries14@gmail.com>
2019-09-10 07:16:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,8 +18,8 @@ class Artist
   end
 
   def add_song_by_name(song_name)
-    
-    #song = Song.new(song_name)
+    song = Song.new(song_name)
+    add_song(song)
     #@songs << song
     #song.artist = self
     #@@song_count += 1
