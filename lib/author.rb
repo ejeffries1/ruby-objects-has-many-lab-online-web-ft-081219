4e2a6469a8a94d7c34a7dca9244579ebaa0a681f
Evan Jeffries <ejeffries14@gmail.com>
2019-09-10 07:30:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -20,7 +20,7 @@ class Author
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post
-    #post.author = self
+    post.author = self
     @@post_count += 1
   end
 
