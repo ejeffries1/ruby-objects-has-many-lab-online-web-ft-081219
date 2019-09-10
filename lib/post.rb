32f35719045bd8bff0c1653fde49e96c3bc0a007
Evan Jeffries <ejeffries14@gmail.com>
2019-09-10 06:52:36 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :title, :author
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+  def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
+  end
+end
